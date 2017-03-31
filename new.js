46f1658282d6e89dc7346a3f0303bb88bdae071b
willyzondri <willyzondri@gmail.com>
2017-03-31 11:53:38 +0700

    Update new.js

--- new.js
+++ new.js
@@ -1,3 +1,18 @@
+$(function() {
+  var menuVisible = false;
+  $('#show-menu').click(function() {
+    if (menuVisible) {
+      $('.navbar-center').css({'display':'none'});
+      $('.navbar-search').css({'display':'none'})
+      menuVisible = false;
+      return;
+    }
+    $('.navbar-center').css({'display':'block'});
+    $('.navbar-search').css({'display':'block'})
+    menuVisible = true;
+  });
+});
+
 if(jQuery("#wpadminbar").length){
 	document.write('<style type="text/css">.fixed-header{top:32px;} @media only screen and (max-width: 782px){.fixed-header{top:46px;}} @media only screen and (max-width: 600px){html #wpadminbar{position:fixed;}}</style>');
 }
