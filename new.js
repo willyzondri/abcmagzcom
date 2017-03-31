337b459ad859f1a7c4c411879255b1575e58cd44
willyzondri <willyzondri@gmail.com>
2017-03-31 08:26:15 +0700

    Update new.js

--- new.js
+++ new.js
@@ -1,1 +1,12 @@
-
+$(function() {
+  var menuVisible = false;
+  $('#show-menu').click(function() {
+    if (menuVisible) {
+      $('.navbar-center').css({'display':'none'});
+      menuVisible = false;
+      return;
+    }
+    $('.navbar-center').css({'display':'block'});
+    menuVisible = true;
+  });
+});
